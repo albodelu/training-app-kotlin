1ff5a7fea878880d39402859b2156172ff76857d
albodelu <albodelu@gmail.com>
2017-04-29 14:01:03 +0200

    Delete comments of second video practices

--- app/src/main/java/com/ardock/training_app_kotlin/Extensions.kt
+++ app/src/main/java/com/ardock/training_app_kotlin/Extensions.kt
@@ -5,7 +5,4 @@ import android.widget.Toast
 
 fun Context.toast(text: CharSequence, length: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, text, length).show()
-
-//fun Context.toast(text: CharSequence) {
-//    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
 }
--- app/src/main/java/com/ardock/training_app_kotlin/Item.kt
+++ app/src/main/java/com/ardock/training_app_kotlin/Item.kt
@@ -1,25 +1,3 @@
 package com.ardock.training_app_kotlin
 
 data class Item(var title: String, var url: String)
-
-//class Item(var title: String, var url: String)
-
-//class Item(title: String, url: String) {
-//
-//    var title: String = title
-//
-//    var url: String = url
-//}
-
-//class Item {
-//
-//    var title: String = ""
-//        get() {
-//            return "title: $field"
-//        }
-//        set(value) {
-//            field = "title: $value"
-//        }
-//
-//    var url: String = ""
-//}
